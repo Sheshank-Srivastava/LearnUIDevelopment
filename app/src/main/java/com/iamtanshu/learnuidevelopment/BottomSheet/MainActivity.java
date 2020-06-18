fa63808393dc4c4a5fc09dfcc58eb2b36da00cf4
Sheshank-Srivastava <ambitious9350164795@gmail.com>
2020-06-18 11:23:08 +0530

    Initial commit

---
+++ app/src/main/java/com/iamtanshu/learnuidevelopment/BottomSheet/MainActivity.java
@@ -0,0 +1,16 @@
+package com.iamtanshu.learnuidevelopment.BottomSheet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.iamtanshu.learnuidevelopment.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
