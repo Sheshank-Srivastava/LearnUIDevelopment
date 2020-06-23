019e69859b5d19be36738a37b38f2d98543a12c6
Sheshank-Srivastava <ambitious9350164795@gmail.com>
2020-06-23 17:25:04 +0530

    Floating Bottom sheet is complete

--- app/src/main/java/com/iamtanshu/learnuidevelopment/BottomSheet/MainActivity.java
+++ app/src/main/java/com/iamtanshu/learnuidevelopment/BottomSheet/MainActivity.java
@@ -6,7 +6,14 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.iamtanshu.learnuidevelopment.BottomSheet.Adapter.UserListAdapter;
 import com.iamtanshu.learnuidevelopment.R;
 
@@ -19,11 +26,27 @@ public class MainActivity extends AppCompatActivity {
     LinearLayoutManager manager;
     List<String> mUserList = new ArrayList<>();
 
+    //bottom Sheet variable
+    BottomSheetBehavior bottomSheetBehavior;
+    BottomSheetDialog bottomSheetDialog;
+    View bottom_sheet, view;
+    TextView txtUserName;
+    ImageView imgClose;
+    Button btn_PingMe;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        setUserList();
+        setRecycler();
+        showFloatingBottomSheet();
+    }
+
+    //Function inflater userlist
+    private void setUserList() {
         mUserList.add("Sheshank Srivastava");
         mUserList.add("Piyush Khanna");
         mUserList.add("Tushar Garg");
@@ -34,17 +57,53 @@ public class MainActivity extends AppCompatActivity {
         mUserList.add("Shivam Shukla");
         mUserList.add("Abhilash Kumar");
         mUserList.add("Rishab Anand");
+    }
+
+    //Function setup recyclerView
+    private void setRecycler() {
 
         recyc_UserList = findViewById(R.id.recyc_UserList);
         manager = new LinearLayoutManager(this);
         adapter = new UserListAdapter(MainActivity.this, mUserList, new UserListAdapter.ItemClickListener() {
             @Override
-            public void onClick(View v,int position) {
-
-
+            public void onClick(View v, int position) {
+                txtUserName.setText(mUserList.get(position));
+                bottomSheetDialog.show();
             }
         });
         recyc_UserList.setLayoutManager(manager);
         recyc_UserList.setAdapter(adapter);
     }
+
+    //Function that set Floating Bottom Sheet
+    public void showFloatingBottomSheet() {
+        bottom_sheet = findViewById(R.id.bottom_sheet);
+        bottomSheetBehavior = BottomSheetBehavior.from(bottom_sheet);
+        view = getLayoutInflater().inflate(R.layout.float_bottom_sheet, null);
+
+        bottomSheetDialog = new BottomSheetDialog(this);
+
+        bottomSheetDialog.setContentView(view);
+        bottomSheetDialog.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        ((View) view.getParent()).setBackgroundColor(getResources().getColor(android.R.color.transparent));
+
+        txtUserName = view.findViewById(R.id.txt_name);
+        imgClose = view.findViewById(R.id.img_close);
+
+        imgClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bottomSheetDialog.dismiss();
+            }
+        });
+        btn_PingMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Ping Me", Toast.LENGTH_SHORT).show();
+                bottomSheetDialog.dismiss();
+            }
+        });
+    }
+
+
 }
